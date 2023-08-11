d76ac865cb93973d3998fd8ebd6302a3f51d5853
Viktor Biruk <vikttur.job@gmail.com>
2023-08-11 13:16:12 +0300

    move DB-Host to process.env

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const app = require('./app')
 const mongoose = require('mongoose');
+const { DB_HOST } = process.env;
 
-const DB_HOST = 'mongodb+srv://viktor:Lc74lMFJekcvbyrK@cluster-vik-0.bm7zdbj.mongodb.net/contacts_base?retryWrites=true&w=majority';
+
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
 	.then(() => {
-		app.listen(3000)
+		app.listen(3000);
+		console.log('Database connection successful');
 	})
 	.catch(error => {
 		console.log(error.message);

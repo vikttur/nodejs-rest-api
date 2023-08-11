503973e593bac65b0875357deb8e979fa1f948b9
Viktor Biruk <vikttur.job@gmail.com>
2023-08-11 09:18:54 +0300

    connects the database to the server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://viktor:Lc74lMFJekcvbyrK@cluster-vik-0.bm7zdbj.mongodb.net/contacts_base?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+	.then(() => {
+		app.listen(3000)
+	})
+	.catch(error => {
+		console.log(error.message);
+		process.exit(1);
+	})
